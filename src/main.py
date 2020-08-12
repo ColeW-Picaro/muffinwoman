835c4b32e1c6c251ff23fec7833acdb5d697e426
colew-picaro <colevohs@gmail.com>
2020-08-12 13:18:50 -0400

    add documentation

--- src/main.py
+++ src/main.py
@@ -1,3 +1,7 @@
+# Filename: main.py
+# Author: Cole Vohs
+# Description: Entry point for muffinwoman.py
+
 import muffinwoman
 
 if __name__ == '__main__':
--- src/muffinwoman.py
+++ src/muffinwoman.py
@@ -1,3 +1,7 @@
+# Filename: muffinwoman.py
+# Author: Cole Vohs
+# Description: Creates a discord bot, initializes commands, and runs it
+
 import os
 import discord
 from dotenv import load_dotenv
@@ -9,21 +13,14 @@ from cogs.music import Music
 class Muffinwoman ():
 
   def __init__(self):
+    # Create Bot and add the cogs
     self.bot = Bot(command_prefix = '.', help_command = None, description = None)
     self.bot.add_cog(Meta(self.bot))
     self.bot.add_cog(Music(self.bot))
 
+    # fetch the token from the environment variable
     load_dotenv()
     token = os.getenv('DISCORD_TOKEN')
 
+    # run the bot
     self.bot.run(token)
-
-
-
-
-
-
-
-
-
-
