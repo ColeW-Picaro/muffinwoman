6e11177ed7be4493ecfe9424419eb5bfb5dc5cde
Nate Ijams <nate@ijams.me>
2020-12-05 20:52:29 -0600

    Add skip and queue features.

--- src/cogs/meta.py
+++ src/cogs/meta.py
@@ -20,7 +20,7 @@ class Meta (Cog):
 
   @command(name = "help")
   async def help(self, ctx):
-      await ctx.send("Muffinwoman Help Page\n---\n.help: This help info.\n.play [url]: Play media from URL and join voice.\n.leave: Stop playing and leave voice channel.\n.join: Force the bot to join your channel.\n.info: Show software, developer, and license info.\n---\nTip: there is a queue! Just run the .play command to add to it.")
+      await ctx.send("Muffinwoman Help Page\n---\n.help: This help info.\n.play [url]: Play media from URL and join voice.\n.leave: Stop playing and leave voice channel.\n.join: Force the bot to join your channel.\n.skip: Skip current song.\n.queue: See the current playlist!\n.clearqueue: Reset queue!\n.info: Show software, developer, and license info.\n---\nTip: there is a queue! Just run the .play command to add to it.")
 
   @command(name = "info")
   async def info(self, ctx):
